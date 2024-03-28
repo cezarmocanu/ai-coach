8571bdeec712c8931c810579866c15405f9f1f0e
malexa21 <aaliroalexandra@gmail.com>
2024-03-28 17:27:49 +0200

    Finished itroducing in ChevronDown.tsx

--- src/shared/components/atoms/ChevronDown/ChevronDown.stories.ts
+++ src/shared/components/atoms/ChevronDown/ChevronDown.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { ChevronDown } from "./ChevronDown";
+import { ChevronDownVariant } from "./ChevronDown";
 
 const meta = {
   title: "atoms/ChevronDown",
@@ -15,4 +15,4 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const ChevronDown: Story = {};
