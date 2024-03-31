b3f5b05aa476db2949b11b03ec3a57e9db2081c3
malexa21 <aaliroalexandra@gmail.com>
2024-03-31 09:21:04 +0300

    I finished adding chevron down variants into storybook

--- src/shared/components/atoms/ChevronDown/ChevronDown.stories.ts
+++ src/shared/components/atoms/ChevronDown/ChevronDown.stories.ts
@@ -1,18 +1,37 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { ChevronDownVariant } from "./ChevronDown";
+import { ChevronDown, ChevronDownVariant } from "./ChevronDown";
 
 const meta = {
   title: "atoms/ChevronDown",
-  component: ChevronDownVariant,
+  component: ChevronDown,
   parameters: {
     layout: "centered",
   },
   tags: ["autodocs"],
   argTypes: {},
   args: {},
-} satisfies Meta<typeof ChevronDownVariant>;
+} satisfies Meta<typeof ChevronDown>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const ChevronDown: Story = {};
+export const Outlined: Story = {
+  args: {
+    variant: ChevronDownVariant.outline,
+ }
+};
+export const Solid: Story = {
+  args: {
+    variant: ChevronDownVariant.solid,
+ }
+}
+export const Mini: Story = {
+  args: {
+    variant: ChevronDownVariant.mini,
+ }
+}
+export const Micro: Story = {
+  args: {
+    variant: ChevronDownVariant.micro,
+ }
+}
